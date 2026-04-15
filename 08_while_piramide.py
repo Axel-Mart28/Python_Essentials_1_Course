d7148d30ceee83f5f56ee740c9b7261e957e491b
Axel-Mart28 <axelxdramos10@gmail.com>
2026-04-15 12:54:43 -0600

    update

---
+++ 08_while_piramide.py
@@ -0,0 +1,11 @@
+print("Bienvenido al programa de piramides")
+
+blocks = int(input("Ingresa el número de bloques: "))
+
+altura_inicial = 0
+capa_por_bloques = 1
+
+while capa_por_bloques <= blocks:
+    blocks = blocks - capa_por_bloques
+
+print("La altura de la pirámide:", altura_inicial)
