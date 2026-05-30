6ce044ed4b010eab5115e863efdc5609de718b00
Axel-Mart28 <axelxdramos10@gmail.com>
2026-05-29 19:54:22 -0600

    bubble sort

---
+++ 20_bubble_sort.py
@@ -0,0 +1,11 @@
+#Ejemplo del uso del ordenamiento de busbuja mediante la instruccion sort
+
+list = [40, 50, 1, 6, 15, 4]
+print(list)
+
+list.sort()
+print(list)
+
+#Invertir el orden de la lista
+list.reverse()
+print(list)
